2670bc52d6dccaeb93400ae1e64cae39e65acf25
mosabberul55 <mosabberulislam55@gmail.com>
2025-01-21 15:55:42 +0600

    redis cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CacheModule } from '@nestjs/cache-manager';
 import { StudentModule } from './student/student.module';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     CacheModule.register({
       isGlobal: true,
       ttl: 30 * 1000,
+      store: redisStore,
     }),
     StudentModule,
   ],
--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, HttpCode, HttpStatus, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseInterceptors,
+} from '@nestjs/common';
 import { StudentService } from './student.service';
-import { CacheInterceptor } from '@nestjs/cache-manager';
+import { CacheInterceptor, CacheKey } from '@nestjs/cache-manager';
 import { CacheTTL } from '@nestjs/common/cache';
 
 @UseInterceptors(CacheInterceptor)
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
+
   @HttpCode(HttpStatus.OK)
-  @CacheTTL(20 * 1000)
+  @CacheTTL(60 * 1000)
+  @CacheKey('students')
   @Get()
   async getStudents() {
     return this.studentService.getStudents();
